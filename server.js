69b9f38613e3dad9f89339b5d11fe2d67f6d2ffe
Kev <pwntastickevin@gmail.com>
2019-08-08 17:30:50 -0600

    HOUSTON. WE HAVE DATA

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema')
+const cors = require('cors')
 
 const app = express();
+
+//Allow Cross origin
+app.use(cors())
+
+// Error handler
+const errorHandler = (err, req, res, next) => {
+	if (res.headersSent) {
+	  return next(err);
+	}
+	const { status } = err;
+	res.status(status).json(err);
+  };
+  app.use(errorHandler);
 
 app.use('/graphql', graphqlHTTP({
 	schema: schema,
